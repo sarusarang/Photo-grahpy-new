069256e4b7f73cd5cebd31cb0fd6c7acc5abb63e
sarusarang <sarangsaru445@gmail.com>
2026-09-10 17:50:43 +0530

    first commit

---
+++ dist/assets/cloud-upload-BD075Yra.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-DlFfz10l.js";var t={name:`cloud-upload`,size:24,node:[[`path`,{d:`M12 13v8`,key:`1l5pq0`}],[`path`,{d:`M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242`,key:`1pljnt`}],[`path`,{d:`m8 17 4-4 4 4`,key:`1quai1`}]],aliases:[`upload-cloud`]};t.node;var n=e(t);export{n as t};
